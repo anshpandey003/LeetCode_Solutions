2c5e92ed49329c31a681a753db9b6b1f14aaf213
Ansh Pandey <100069507+anshpandey003@users.noreply.github.com>
2025-02-22 16:17:17 +0530

    Time: 5 ms (73.01%), Space: 43.8 MB (94.9%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,3 +6,13 @@ class Solution {
         }
         int reverse=0;
         int temp=x;
+
+        while(x>0)
+        {
+            reverse=(reverse*10)+(x%10);
+            x/=10;
+        }
+
+        return reverse==temp;
+    }
+}
