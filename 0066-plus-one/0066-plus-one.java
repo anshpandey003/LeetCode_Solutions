ca33ab8117b27a9113935c36011925cd5f45f669
Ansh Pandey <100069507+anshpandey003@users.noreply.github.com>
2025-03-26 20:15:32 +0530

    Time: 0 ms (100%), Space: 41.7 MB (81.9%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean isNine = true;
+        for(int x=0;x<digits.length;x++){
+            if(digits[x]!=9){
+                isNine = false;
+            }
+        }
+        int ar[] = new int[digits.length+1];
+        if(isNine){
+            ar[0] = 1;
+            for(int x=1;x<ar.length;x++){
+                ar[x] = 0;
+            }
+        }else{
+            if(digits[digits.length-1]==9){
+                // digits[digits.length-1] = 0;
+                int x=digits.length-1;
+                while(digits[x]==9){
+                    digits[x]=0;
+                    x--;
+                }
+                digits[x]+=1;
+            }else{
+                digits[digits.length-1]+=1;
+            }
+        }
+        if(isNine){
+            return ar;
+        }else{
+            return digits;
+        }
+    }
+}
